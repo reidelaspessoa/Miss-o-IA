a2464e94f7c31ad0999221109a01ed2e4ca8e3c8
reidelaspessoa <107411087+reidelaspessoa@users.noreply.github.com>
2024-07-02 08:18:17 -0300

    Add files via upload

---
+++ Miss-o-IA-main/MISSAO_IA/script.js
@@ -0,0 +1,41 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas'); 
+const caixaAlternativas = document.querySelector('.caixa-alternativas');   
+const caixaResultado = document.querySelector('.caixa-resultado'); 
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+    enunciado: "Qual país tem o maior índice de pobreza?",
+    alternativas: [
+        {
+            texto:"África", 
+            afirmação:"Afirmação da alternativa 1"
+        },
+        { "Somália", "Suíça", "Cuba",}
+    {
+    enunciado: "Qual o estado brasileiro mais racista?",
+    alternativas: ["São Paulo", "Paraná", "Rio Granda do Sul", "Santa Catarina"],
+    },
+    {
+    enunciado: "Qual dos seguintes fatores é mais frequentemente citado como uma causa primária da desigualdade social em sociedades modernas?",
+    alternativas: ["Distribuição desigual de riqueza e renda", "Localização geográfica", "Preferências culturais", "Condições climáticas"],
+    },
+    ];
+
+    let atual = 0;
+    let perguntaAtual;
+    function mostraPergunta() {
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado
+        mostraAlternativas();
+        }
+        function mostraAlternativas() {
+            for (const alternativa of perguntaAtual.alternativas) {
+            const botaoAlternativas = 
+            document.createElement("button");
+            botaoAlternativas.textContent = alternativa;
+            caixaAlternativas.appendChild(botaoAlternativas);
+            }
+        }
+        mostraPergunta();
