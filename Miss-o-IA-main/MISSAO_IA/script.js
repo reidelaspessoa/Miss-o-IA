de6bf8b0be5829e085bf55455114a75e8e852eb8
reidelaspessoa <107411087+reidelaspessoa@users.noreply.github.com>
2024-07-30 09:04:05 -0300

    Add files via upload

--- Miss-o-IA-main/MISSAO_IA/script.js
+++ Miss-o-IA-main/MISSAO_IA/script.js
@@ -10,32 +10,99 @@ const perguntas = [
     alternativas: [
         {
             texto:"África", 
-            afirmação:"Afirmação da alternativa 1"
+            afirmação:""
         },
-        { "Somália", "Suíça", "Cuba",}
+        { 
+            texto: "Somália", 
+            afirmação: ""
+        },
+        {
+            texto: "Suíça",
+            afirmação: "" 
+        },
+        {
+            texto: "Cuba",
+            afirmação: ""
+        }
+    ]
+},
     {
     enunciado: "Qual o estado brasileiro mais racista?",
-    alternativas: ["São Paulo", "Paraná", "Rio Granda do Sul", "Santa Catarina"],
-    },
+    alternativas: [
+        {
+            texto: "São Paulo", 
+            afirmação: "" 
+        },
+        {
+            texto: "Paraná", 
+            afirmação: "" 
+        },
+        {
+            texto: "Rio Granda do Sul", 
+            afirmação: "" 
+        },
+        {
+            texto: "Santa Catarina",
+            afirmação: ""
+        }
+    ]
+},
     {
     enunciado: "Qual dos seguintes fatores é mais frequentemente citado como uma causa primária da desigualdade social em sociedades modernas?",
-    alternativas: ["Distribuição desigual de riqueza e renda", "Localização geográfica", "Preferências culturais", "Condições climáticas"],
-    },
+    alternativas: [
+        {
+            texto: "Distribuição desigual de riqueza e renda", 
+            afirmação: "" 
+        },
+        {
+            texto: "Localização geográfica", 
+            afirmação: "" 
+        },
+        {
+            texto: "Preferências culturais", 
+            afirmação: "" 
+        },
+        {
+            texto: "Condições climáticas",
+            afirmação: ""
+        }
+    ]
+},
     ];
 
     let atual = 0;
     let perguntaAtual;
+    let historiaFinal = "";
+
     function mostraPergunta() {
+        if (atual >= perguntas.length) {
+            mostraResultado();
+            return;
+
+        }
         perguntaAtual = perguntas[atual];
-        caixaPerguntas.textContent = perguntaAtual.enunciado
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        caixaAlternativas.textContent = " ";
         mostraAlternativas();
         }
         function mostraAlternativas() {
             for (const alternativa of perguntaAtual.alternativas) {
             const botaoAlternativas = 
             document.createElement("button");
-            botaoAlternativas.textContent = alternativa;
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
             caixaAlternativas.appendChild(botaoAlternativas);
             }
         }
+        function mostraResultado() {
+            caixaPerguntas.textContent = "As perguntas abordam temas relevantes sobre desigualdade social: o primeiro questiona qual país possui o maior índice de pobreza e o segundo busca identificar o estado brasileiro mais racista. A última questão se concentra em investigar qual dos fatores, dentre os apresentados, é mais frequentemente apontado como causa primária da desigualdade social em sociedades modernas.";
+            textoResultado.textContent = historiaFinal;
+            caixaAlternativas.textContent = " ";
+            }
+        function respostaSelecionada(opcaoSelecionada) {
+            const afirmacoes = opcaoSelecionada.afirmação;
+            historiaFinal = afirmacoes + " ";
+            atual++;
+            mostraPergunta();
+            }
         mostraPergunta();
